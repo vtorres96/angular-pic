e2dc7ebfc2048f74dd8f2f9f1e5a49508b078821
vtorres96 <victorcdc96@gmail.com>
2020-06-29 22:59:05 -0300

    added photos module

---
+++ web/src/app/photos/photos.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { PhotoComponent } from './components/photo/photo.component';
+
+@NgModule({
+  declarations: [PhotoComponent],
+  exports: [PhotoComponent],
+})
+export class PhotosModule {}
