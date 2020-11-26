9ae817b9ed44b220f576f509aa287e7fa0f55cac
vtorres96 <victorcdc96@gmail.com>
2020-11-26 11:36:31 -0300

    added components: app

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -3,19 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  photos = [
-    {
-      url:
-        'https://ichef.bbci.co.uk/news/1024/branded_news/1CE8/production/_109100470_ed01bd7f-0c64-4f6b-9d29-f43261119ade.jpg',
-      description: 'Leão',
-    },
-    {
-      url:
-        'https://ichef.bbci.co.uk/news/1024/branded_news/1CE8/production/_109100470_ed01bd7f-0c64-4f6b-9d29-f43261119ade.jpg',
-      description: 'Leão',
-    },
-  ];
-}
+export class AppComponent { }
