9a6a9d407f9201ba7b30f2d059e00b8510726b71
vtorres96 <victorcdc96@gmail.com>
2020-12-15 22:45:02 -0300

    added app module

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+// Modules
+import { AppRoutingModule } from './app.routing.module';
+import { PhotosModule } from './photos/photos.module';
+import { ErrorsModule } from './errors/errors.module';
+import { HomeModule } from './home/home.module';
+import { CoreModule } from './core/core.module';
+
+// Components
 import { AppComponent } from './app.component';
 
-import { PhotosModule } from './photos/photos.module';
-import { AppRoutingModule } from './app.routing.module';
-import { ErrorsModule } from './errors/errors.module';
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    PhotosModule,
-    AppRoutingModule,
-    ErrorsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        CoreModule,
+        ErrorsModule,
+        PhotosModule,
+        HomeModule
+    ],
+    providers: [],
+    bootstrap: [
+        AppComponent
+    ]
 })
-export class AppModule { }
+export class AppModule {
+}
