f3b0f62d0bd4229af905e2d97a78ff50ae6e739a
vtorres96 <victorcdc96@gmail.com>
2020-12-15 22:37:46 -0300

    added photo-list components: photos

--- web/src/app/photos/photo-list/photos/photos.component.ts
+++ web/src/app/photos/photo-list/photos/photos.component.ts
@@ -1,30 +1,34 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-
 import { Photo } from '../../photo/photo';
 
 @Component({
-  selector: 'ap-photos',
-  templateUrl: './photos.component.html',
-  styleUrls: ['./photos.component.css']
+    selector: 'ap-photos',
+    templateUrl: './photos.component.html',
+    styleUrls: [ './photos.component.css' ]
 })
 export class PhotosComponent implements OnChanges {
-  
-  @Input() photos: Photo[] = [];
-  rows: any[] = [];
-  
-  constructor() { }
-  
-  ngOnChanges(changes: SimpleChanges) {
-    if(changes.photos) 
-      this.rows = this.groupColumns(this.photos);
-  }
 
-  groupColumns(photos: Photo[]) {
-    const newRows = [];
+    @Input()
+    public photos: Photo[];
 
-    for(let index = 0; index < photos.length; index+=3) {
-      newRows.push(photos.slice(index, index + 3));
-    }                            
-    return newRows;
-  }
+    public rows: Photo[][];
+
+    constructor() {
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes.photos) {
+            this.rows = this.getGroupColumns(this.photos);
+        }
+    }
+
+    private getGroupColumns(photos: Photo[]): Photo[][] {
+        const rows = [];
+
+        for (let index = 0; index < photos.length; index += 3) {
+            rows.push(photos.slice(index, index + 3));
+        }
+
+        return rows;
+    }
 }
