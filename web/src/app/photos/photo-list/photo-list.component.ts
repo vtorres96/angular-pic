0868ab4a233828dc9000457a2bf836c389671468
vtorres96 <victorcdc96@gmail.com>
2020-12-15 22:29:20 -0300

    added photos components: photo-list

--- web/src/app/photos/photo-list/photo-list.component.ts
+++ web/src/app/photos/photo-list/photo-list.component.ts
@@ -1,48 +1,47 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
-  selector: 'app-photo-list',
-  templateUrl: './photo-list.component.html',
-  styleUrls: ['./photo-list.component.css']
+    selector: 'ap-photo-list',
+    templateUrl: './photo-list.component.html',
+    styleUrls: [ './photo-list.component.css' ]
 })
-export class PhotoListComponent implements OnInit, OnDestroy {
+export class PhotoListComponent implements OnInit {
 
-  photos: Photo[] = [];
-  filter: string = '';
-  debounce: Subject<string> = new Subject<string>();
-  hasMore: boolean = true;
-  currentPage: number = 1;
-  userName: string = '';
 
-  constructor(
-    private activatedRoute: ActivatedRoute,
-    private photoService: PhotoService
-  ) { }
+    public filter = '';
+    public hasMore = true;
+    public currentPage = 1;
+    public username = '';
+    public photos: Photo[] = [];
 
-  ngOnInit(): void {
-    this.userName = this.activatedRoute.snapshot.params.userName;
-    this.photos = this.activatedRoute.snapshot.data['photos'];
-    this.debounce
-      .pipe(debounceTime(300))
-      .subscribe(filter => this.filter = filter);
-  }
+    public constructor(private activatedRoute: ActivatedRoute,
+                       private photoService: PhotoService) {
+    }
 
-  ngOnDestroy(): void {
-    this.debounce.unsubscribe();
-  }
+    public ngOnInit(): void {
+        this.username = this.activatedRoute.snapshot.params.username;
+        this.photos = this.activatedRoute.snapshot.data.photos;
 
-  load() {
-    this.photoService
-      .listFromUserPaginated(this.userName, ++this.currentPage)
-      .subscribe(photos => {
-        this.photos = this.photos.concat(photos);
-        if(!photos.length) this.hasMore = false;
-      });
-  }
+    }
+
+
+    /**
+     * Load more photos with pagination.
+     */
+    public loadPhotos(): void {
+        this.photoService
+            .listFromUserPaginated(this.username, ++this.currentPage)
+            .subscribe(photos => {
+                this.filter = '';
+                this.photos = this.photos.concat(photos);
+
+                if (!photos.length) {
+                    this.hasMore = false;
+                }
+            });
+    }
 }
