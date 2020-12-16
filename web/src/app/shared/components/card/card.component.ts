be8aa9a24969d7fa8773385ff9df7b03a7253c3c
vtorres96 <victorcdc96@gmail.com>
2020-12-15 22:40:29 -0300

    added shared components: card

---
+++ web/src/app/shared/components/card/card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'ap-card',
+    templateUrl: './card.component.html',
+    styleUrls: [ './card.component.css' ]
+})
+export class CardComponent implements OnInit {
+
+    @Input()
+    public title = '';
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
